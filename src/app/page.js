2894cd0286dbfc05f0adb8c57b7304aba7665bbf
khalilalmasri <faresalharamy@gmail.com>
2024-06-26 09:37:23 +0300

    Refactor freelancer page component import in Home page component

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,5 @@
-import IndependenceFleelancer from "@/components/freelancer/independence-section-freelancer";
-import Hrc from "@/components/freelancer/hr";
-import First_section from "@/components/freelancer/first-section";
-import Second_section from "@/components/freelancer/second-section-f";
-import Thierd_section from "@/components/freelancer/thierd-section";
-import Forth_section from "@/components/freelancer/forth-section";
-import Form_box from "@/components/form-box";
+import FreelancerPage from "./freelancer/page";
 
 export default function Home() {
-  return (
-    <div >
-      <IndependenceFleelancer />
-      <Hrc />
-      <First_section />
-      <Second_section />
-      <Thierd_section />
-      <Forth_section />
-      <Form_box />
-    </div>
-  );
+  return <FreelancerPage />;
 }
