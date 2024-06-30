14b8d54392637fc70ef36961fb27d935fd8cadc2
khalilalmasri <faresalharamy@gmail.com>
2024-06-30 16:47:20 +0300

    Refactor HrB component layout and font sizes
    This commit updates the layout and font sizes of the HrB component to improve its visual appearance and readability. The heading text is now split into three lines and the font size has been reduced for smaller screens. The horizontal line has also been adjusted to better align with the heading text.

--- src/components/business/hrb.jsx
+++ src/components/business/hrb.jsx
@@ -1,11 +1,15 @@
 const HrB = () => {
   return (
-    <div className=" SamsungOne-bold m-3 sm:my-12 sm:me-16 sm:ms-12 text-3xl sm:text-5xl text-[#040404] flex">
-      <h1 className="w-1/2  sm:w-1/3">Your Success Partners </h1>
+    <div className=" SamsungOne-bold m-3 sm:my-12 sm:me-16 sm:ms-12 text-lg sm:text-3xl text-[#040404] flex">
+      <div className="w-full  sm:w-2/5">
+        <h1 className="mb-2">Your development , </h1>
+        <h1 className="my-2"> expansion and </h1>
+        <h1 className="my-2"> success partner </h1>
+      </div>
       <div className="w-full  sm:ms-5 sm:my-4 ">
         {" "}
         <div className="h-full "> </div>
-        <hr className="border-[1px] sm:border-2 mt-[-8px] sm:mt-0 ms-4 sm:ms-[-20px] border-[#040404] " />
+        <hr className="border-[1px] sm:border-2 mt-[-15px] sm:mt-0  sm:ms-[-20px] border-[#040404] " />
       </div>
     </div>
   );
